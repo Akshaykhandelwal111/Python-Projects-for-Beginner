b03582e2f42a7006875713b3a65b39050f448106
akshay khandelwal <64717539+Akshaykhandelwal111@users.noreply.github.com>
2022-10-02 16:47:56 +0530

    Create Dice Roller.py

---
+++ Dice Roller/Dice Roller.py
@@ -0,0 +1,16 @@
+#Importing randome module
+import random
+
+# Initiating an white loop to keep the program execting
+while True:
+  print("Rolling Dice...")
+  
+  #using random.randint(1,6)) to generate a random value between 1 & 6
+  print(f"The value is , random.randint(1,6))
+  
+  # Asking user to roll the dice again or quit
+  repeat = input("Roll Dice again? 'y' for yes & 'n' for no: "0)
+        
+  # if the user answers negative the loop will break and program execution stops otherwise the program will continue executing
+   if repeat == 'n':
+       break
