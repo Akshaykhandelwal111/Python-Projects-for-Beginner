d8d841b72a91980f78ca24aa622447f48ebccb09
akshay khandelwal <64717539+Akshaykhandelwal111@users.noreply.github.com>
2022-10-02 16:31:30 +0530

    Create Color Text Printer.py

---
+++ Color Text Printer/Color Text Printer.py
@@ -0,0 +1,17 @@
+import colorama
+# Back for background, Fore fro text color
+from colorama import Back, Fore
+
+# this line is make sure that the color style restes once the execution of program is complete
+colorama.init(autoreset =True)
+text = input("Enter a pharse or sentence:")
+
+#Colorama has limited color options
+print(Fore.BLACK + Back.WHITE + text)
+print(Fore.RED + Back.CYAN + text)
+print(Fore.GREEN + Back.MAGENTA + text)
+print(Fore.YELLOW + Back.BLUE + text)
+print(Fore.BLUE + Back.YELLOW + text)
+print(Fore.MAGENTA + Back.GREEN + text)
+print(Fore.CYAN + Back.RED + text)
+print(Fore.WHITE + Back.BLACK + text)
