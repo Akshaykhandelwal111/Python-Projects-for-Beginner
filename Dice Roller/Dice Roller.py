f2d9a4d19d919b32cc99c4cd9d843a1817018bc3
akshay khandelwal <64717539+Akshaykhandelwal111@users.noreply.github.com>
2022-10-02 16:52:24 +0530

    Update Dice Roller.py

--- Dice Roller/Dice Roller.py
+++ Dice Roller/Dice Roller.py
@@ -6,7 +6,7 @@ while True:
   print("Rolling Dice...")
   
   #using random.randint(1,6)) to generate a random value between 1 & 6
-  print(f"The value is , random.randint(1,6))
+  print(f"The value is ", random.randint(1,6))
   
   # Asking user to roll the dice again or quit
   repeat = input("Roll Dice again? 'y' for yes & 'n' for no: "0)
